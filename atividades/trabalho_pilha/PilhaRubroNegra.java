e3e3d621eaafa9ed30f41a4a50a0a5dccf9493d7
sarahbeatriz0205 <sarah.nascimento02052007@gmail.com>
2026-03-23 17:36:03 -0300

    terminando trabalho de pilha. revisar amanha

---
+++ atividades/trabalho_pilha/PilhaRubroNegra.java
@@ -0,0 +1,108 @@
+public class PilhaRubroNegra{
+    private int capacidade;
+    private Object[] a;
+    private int tVermelho;
+    private int tPreto;
+
+    public PilhaRubroNegra(int capacidade){
+        this.capacidade = capacidade;
+        tVermelho = -1;
+        tPreto = capacidade; // tamanho do array
+        a = new Object[capacidade];
+    }
+
+    public class PilhaVaziaExcecao extends RuntimeException{
+        public PilhaVaziaExcecao(String error){
+            super(error);
+        }
+    }
+
+    private int sizeVermelho() {
+        return tVermelho + 1;
+    }
+
+    private int sizePreto(){
+        return capacidade - tPreto; // subtraindo a capacidade pelo indice atual do preto, dá a qtd de elementos da pilha preta
+    }
+
+    private int qtdElementos(){
+        return sizeVermelho() + sizePreto();
+    }
+
+    private void realocarElementosArray(){
+        Object b[] = new Object[capacidade];
+        for (int f=0; f <= tVermelho; f++){
+            b[f] = a[f];
+        }
+        int qtdPretos = a.length - tPreto;
+        int novoTPreto = capacidade - qtdPretos;
+        int indiceNovoAux = novoTPreto;
+        for (int i = 0; i < qtdPretos; i++){
+            b[indiceNovoAux] = a[tPreto + i];
+            indiceNovoAux++;
+        }
+        a = b;
+        tPreto = novoTPreto;
+    }
+
+    public void pushVermelho(Object o){
+        if (qtdElementos() >= capacidade){
+            capacidade *= 2;
+            realocarElementosArray();
+        }
+        a[++tVermelho] = o;
+    }
+
+    public void pushPreto(Object o){
+        if (qtdElementos() >= capacidade){
+            capacidade *= 2;
+            realocarElementosArray();
+        }
+        a[--tPreto] = o;
+    }
+
+    public Object popVermelho() throws PilhaVaziaExcecao{
+        if (isEmptyVermelho()){
+            throw new PilhaVaziaExcecao("Pilha vermelha vazia");
+        }
+        Object elemento = a[tVermelho--];
+        if (qtdElementos() * 3 <= capacidade && capacidade > 3){ // capacidade > 3 pra que o array não fique muito pequeno 
+            capacidade /= 2;
+            realocarElementosArray();
+        }
+        return elemento;
+    }
+
+    public Object popPreto() throws PilhaVaziaExcecao{
+        if (isEmptyPreto())
+            throw new PilhaVaziaExcecao("Pilha preta vazia");
+        Object elemento = a[tPreto++];
+        if (qtdElementos() * 3 <= capacidade && capacidade > 3){
+            capacidade /= 2;
+            realocarElementosArray();
+        }
+        return elemento;
+    }
+
+    public boolean isEmptyVermelho(){
+        return tVermelho == -1;
+    }
+
+    public boolean isEmptyPreto(){
+        return tPreto == capacidade;
+    }
+
+    public Object topVermelho() throws PilhaVaziaExcecao{
+        if (isEmptyVermelho()){
+            throw new PilhaVaziaExcecao("Pilha vermelha vazia");
+        }
+        return a[tVermelho];
+    }
+
+    public Object topPreto() throws PilhaVaziaExcecao{
+        if (isEmptyPreto()){
+            throw new PilhaVaziaExcecao("Pilha preta vazia");
+        }
+        return a[tPreto];
+    }
+}
---
+++ atividades/trabalho_pilha/TestePilha.java
@@ -0,0 +1,41 @@
+public class TestePilha {
+
+	public static void main(String[] args) {		
+		Integer[] b = new Integer[1];		
+		PilhaRubroNegra pp=new PilhaRubroNegra(1);
+		System.out.println("Inserindo na pilha vermelha");
+		for(int f=0;f<10;f++){
+		  System.out.println(f);		  
+		  pp.pushVermelho(f);
+		}
+        
+        System.out.println();
+        System.out.println("Inserindo na pilha preta");
+        for(int j=0;j<10;j++){
+		  System.out.println(j);		  
+		  pp.pushPreto(j);
+		}
+
+        System.out.println();
+        System.out.println("Retornando o elemento do topo da pilha vermelha sem remover");
+		System.out.println(pp.topVermelho());
+
+        System.out.println();
+        System.out.println("Retornando o elemento do topo da pilha preta sem remover");
+		System.out.println(pp.topPreto());
+
+        System.out.println();
+		System.out.println("Retirando da pilha vermelha");
+		for(int k=0;k<10;k++){
+              System.out.println(k);	
+			  System.out.println("- "+pp.popVermelho());
+		}
+
+        System.out.println();
+        System.out.println("Retirando da pilha preta");
+        for(int i=0;i<10;i++){
+              System.out.println(i);	
+			  System.out.println("- "+pp.popPreto());
+		}
+	}
+}
